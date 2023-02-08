cbcd0fbddcdd0358fabc5f63b75ef6ce64e6b008
Olgunya78 <karaulshchikova@mail.ru>
2023-02-08 20:55:14 +0500

    add Example_005_ifelse_Project

---
+++ Example_005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userneme = Console.ReadLine();
+
+if(userneme.ToLower() == "маша")
+{
+  Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(userneme);
+}
