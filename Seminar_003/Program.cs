22ec7543089ff88d42b24d8045b13094fc7d26ca
Olgunya78 <karaulshchikova@mail.ru>
2023-02-13 18:47:59 +0500

    add Seminar_003_Practice

---
+++ Seminar_003/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите число от 1 до 7: ");
+int nam = Convert.ToInt32(Console.ReadLine());
+if (nam == 1)
+{
+  Console.Write("Понедельник ");
+}
+if (nam == 2)
+{
+    Console.Write("Вторник");
+}
+if (nam == 3)
+{
+    Console.Write("Среда");
+}
+if (nam == 4)
+{
+    Console.Write("Четверг");
+}
+if (nam == 5)
+{
+    Console.Write("Пятница");
+}
+if (nam == 6)
+{
+    Console.Write("Суббота");
+}
+if (nam == 7)
+{
+    Console.Write("Воскресение");
+}
+if (nam > 7)
+{
+    Console.Write("Ошибка");
+}
