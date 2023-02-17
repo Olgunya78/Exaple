5c388066aa7fb359e7e01b8ba742d5f66fe3dd13
Olgunya78 <karaulshchikova@mail.ru>
2023-02-17 13:11:46 +0500

    add_Seminar_Den_Task_002_Task_2

---
+++ Seminar_Den_Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу вычисления значения функции возведения числа в квадрат.
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int Power(int x)
+{
+	return x * x;
+}
+Console.WriteLine(Power(x));
