8e8ade8bf7b5be650f55c12b19f3df1826e6c2a6
Olgunya78 <karaulshchikova@mail.ru>
2023-02-21 15:07:49 +0500

    add Seminar_PZ_003_Task_003

--- Seminar_PZ_002/Program.cs
+++ Seminar_PZ_002/Program.cs
@@ -3,16 +3,70 @@
 // 44 5 78 ->78
 // 22 3 9 -> 22
 
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// if (a > b) 
+// {
+// if (a > c){Console.WriteLine($"Максимальное число {a}");}
+// else {Console.WriteLine($"Максимальное число {c}");}
+// }
+// else if (b > c) {Console.WriteLine($"Максимальное число {b}");}
+// else Console.WriteLine($"Максимальное число {c}");
+
+
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine($"Максимальное число {max}");
+
+
 Console.WriteLine("Введите число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число c: ");
 int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число d: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число e: ");
+int e = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число g: ");
+int g = Convert.ToInt32(Console.ReadLine());
 if (a > b) 
 {
 if (a > c){Console.WriteLine($"Максимальное число {a}");}
 else {Console.WriteLine($"Максимальное число {c}");}
+if (a > d){Console.WriteLine($"Максимальное число {a}");}
+else {Console.WriteLine($"Максимальное число {d}");}
+if (a > e){Console.WriteLine($"Максимальное число {a}");}
+else {Console.WriteLine($"Максимальное число {e}");}
+if (a >g){Console.WriteLine($"Максимальное число {a}");}
+else {Console.WriteLine($"Максимальное число {g}");}
 }
-else if (b > c) {Console.WriteLine($"Максимальное число {b}");}
+{if (b > c) {Console.WriteLine($"Максимальное число {b}");}
 else Console.WriteLine($"Максимальное число {c}");
+if (b > d) {Console.WriteLine($"Максимальное число {b}");}
+else Console.WriteLine($"Максимальное число {d}");
+if (b > e) {Console.WriteLine($"Максимальное число {b}");}
+else Console.WriteLine($"Максимальное число {e}");
+if (b > g) {Console.WriteLine($"Максимальное число {b}");}
+else Console.WriteLine($"Максимальное число {g}");
+}
+{if (d > e) {Console.WriteLine($"Максимальное число {d}");}
+else Console.WriteLine($"Максимальное число {e}");
+if (d > g) {Console.WriteLine($"Максимальное число {d}");}
+else Console.WriteLine($"Максимальное число {g}");
+}
+{if (e > g) {Console.WriteLine($"Максимальное число {e}");}
+else Console.WriteLine($"Максимальное число {g}");}
